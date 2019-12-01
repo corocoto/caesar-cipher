f6f9de784a510f12c3b8e1e8a9bbe4137fecb476
Artem Gusev <gusev2014russia@mail.ru>
2019-12-01 17:30:20 +0300

    added jsdoc documentation

--- cipher.js
+++ cipher.js
@@ -1,6 +1,47 @@
+/**
+ * @version v1.0.0
+ * @file Caesar cipher implementation on node.js
+ * @author Artem Gusev <gusev2014russia@mail.ru> (CreativeRusBear)
+ * @copyright Artem Gusev 2019
+ * @licence
+ * MIT License
+ *
+ * Copyright (c) 2019 Artem Gusev
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
 const inquirer = require('inquirer');
 const chalk = require('chalk');
+
+
+/**
+ * Run cipher program
+ * @example
+ * const program = new RunCipher();
+ * program.answerTheQuestions();
+ */
 class RunCipher {
+	/**
+	 * @description Output starter text, creates array of objects
+	 */
 	constructor () {
 		console.log(chalk.cyan('Hello, You run Cipher of Caesar program.'));
 		console.log(chalk.cyan('Follow next instructions.'));
@@ -19,6 +60,10 @@ class RunCipher {
 		];
 	}
 
+	/**
+	 * @description Method, that ask the questions, and run `getRes` method if answers have been correct
+	 * @return {Promise<void>} get answers on questions
+	 */
 	async answerTheQuestions () {
 		const {text, number} = await inquirer.prompt(this.questions);
 		if (!text.trim() || !number.trim() || !parseInt(number.trim())) {
@@ -29,6 +74,11 @@ class RunCipher {
 		}
 	}
 
+	/**
+	 * @description Encrypt the received text
+	 * @param text {String} Text, that we want to cipher
+	 * @param number {Number} Shift by a certain number of characters
+	 */
 	getRes (text, number) {
 		while (number >= text.length) {
 			number-=text.length;
